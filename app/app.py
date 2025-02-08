d35d15a7512b717b6d1bde2c6aeca80dfa700315
Harsh pratap Singh <harsh.singh@chainstack.com>
2025-02-08 22:42:47 +0530

    Remove custom metrics

--- app/app.py
+++ app/app.py
@@ -6,23 +6,6 @@ import time
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-# Define metrics correctly using the metrics object
-@metrics.counter('requests_total', 'Total requests')
-def count_requests():
-    return 1
-
-@metrics.histogram('request_latency_seconds', 'Request latency in seconds')
-def track_latency():
-    return time.time() - request.start_time if hasattr(request, 'start_time') else 0
-
-@app.before_request
-def before_request():
-    request.start_time = time.time()
-
-@app.after_request
-def after_request(response):
-    return response
-
 @app.route('/health', methods=['GET', 'HEAD', 'POST'])
 def health():
     if request.method in ['GET', 'HEAD']:
